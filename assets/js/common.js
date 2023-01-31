0760d9429c094812e8aea654ec0bde93c12dde8d
seobin <seobin@mcircle.biz>
2023-01-31 09:53:46 +0900

    모바일 헤더 추가

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,31 @@
+// 인클루드 코드 (header.html, footer.html) // 추후 불필요 시 삭제
+window.addEventListener('load', function() {
+    var allElements = document.getElementsByTagName('*');
+    Array.prototype.forEach.call(allElements, function(el) {
+        var includePath = el.dataset.includePath;
+        if (includePath) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    el.outerHTML = this.responseText;
+                }
+            };
+            xhttp.open('GET', includePath, true);
+            xhttp.send();
+        }
+    });
+});
+
+// 디바이스 확인
+function isMobile(){
+    const userAgent = navigator.userAgent;
+    if (userAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || userAgent.match(/LG|SAMSUNG|Samsung/) != null) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 $(function(){
     // 모달창 열기
     var $btnModal = $(".btn-modal");
@@ -11,14 +39,31 @@ $(function(){
     });
     
     // 모달창 닫기
-    var $modalClose = $("button").hasattr("data-dismiss","modal");
-    $modalClose.on("click", function(){
+    // var $modalClose = $("button").hasattr("data-dismiss","modal");
+    // $modalClose.on("click", function(){
         // if($(this).attr("") == "modal"){
-            alert("d");
+            // alert("d");
         // $(this).parents(".modal").hide();
         // }
         
-    });
+    // });
 
+    if(isMobile()) {
+        // mobile only code
+        console.log("mobile");
+    } else {
+        // pc only code
+        console.log("pc");
+        // $("header .main-menu").overmouse(function(){
+        //     $(this).addClass("active");
+        // }).leavemouse(function(){
+        //     $(this).removeClass("active");
+        // });
+    }
+
+    // 모바일 | 헤더메뉴 열기
+    $(".btn-nav-menu").on("click", function(){
+        $(this).toggleClass("active");
+    });
 
 });
